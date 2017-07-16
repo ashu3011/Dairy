3e8124fb43cb19b3ee5078f5306cc8abcdc5efe2
Isha Sharma <isha781sharma@gmail.com>
2017-07-16 10:41:35 -0700

    dairy app

---
+++ app/src/main/java/com/example/ashutosh/dairy/MainDairy.java
@@ -0,0 +1,13 @@
+package com.example.ashutosh.dairy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainDairy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_dairy);
+    }
+}

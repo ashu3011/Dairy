dd011e22d9fd3c1e0566189b8af0766f5756d925
Isha Sharma <isha781sharma@gmail.com>
2017-07-30 09:48:37 -0700

    login

--- app/src/main/java/com/example/ashutosh/dairy/LoginRegister.java
+++ app/src/main/java/com/example/ashutosh/dairy/LoginRegister.java
@@ -1,7 +1,12 @@
 package com.example.ashutosh.dairy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginRegister extends AppCompatActivity {
 
@@ -9,5 +14,19 @@ public class LoginRegister extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_register);
+
+        final EditText eUname =(EditText) findViewById(R.id.eUname);
+        final EditText ePassword =(EditText) findViewById(R.id.ePassword);
+
+        final Button Login =(Button) findViewById(R.id.login);
+        final TextView registerLink =(TextView) findViewById(R.id.tvRegisterHere);
+        registerLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent regiterIntent = new Intent(LoginRegister.this, RegisterActivity.class);
+                LoginRegister.this.startActivity(regiterIntent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/ashutosh/dairy/RegisterActivity.java
+++ app/src/main/java/com/example/ashutosh/dairy/RegisterActivity.java
@@ -2,6 +2,8 @@ package com.example.ashutosh.dairy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        final EditText eAge =(EditText) findViewById(R.id.eAge);
+        final EditText eName =(EditText) findViewById(R.id.eName);
+        final EditText eUname =(EditText) findViewById(R.id.eUname);
+        final EditText ePassword =(EditText) findViewById(R.id.ePassword);
+
+        final Button Register =(Button) findViewById(R.id.Register);
+
+
     }
 }
